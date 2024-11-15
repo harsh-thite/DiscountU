f6875295af95df26df926524b7d2c5a225e1c5f5
Harsh Thite <harshthite2003@gmail.com>
2024-11-15 12:22:11 +0530

    Some updates.

--- student_offers/admin.py
+++ student_offers/admin.py
@@ -4,7 +4,7 @@ from .models import Category, Discount
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'order', 'is_active')
     prepopulated_fields = {'slug': ('name',)}
-    fields = ('name', 'slug', 'description', 'icon', 'image', 'is_active', 'order')  # Add 'image' here
+    fields = ('name', 'slug', 'description', 'icon', 'image', 'is_active', 'order')  # Add 'image'
 
 class DiscountAdmin(admin.ModelAdmin):
     list_display = ('title', 'company_name', 'category', 'status', 'valid_from', 'valid_until')
