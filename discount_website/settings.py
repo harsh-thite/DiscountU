df02bed0888b1021c17248ef814d3919aa454a9a
Harsh Thite <harshthite2003@gmail.com>
2025-02-10 13:32:04 +0530

    Some updates.

--- discount_website/settings.py
+++ discount_website/settings.py
@@ -64,7 +64,8 @@ WSGI_APPLICATION = 'discount_website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/var/lib/sqlite/db.sqlite3',  # Storing SQLite in persistent storage
+       # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
